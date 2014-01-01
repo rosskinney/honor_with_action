e1a0ebced8fe47ceb8cc54bc73c6f9bf7e0a334b
MakerSquare Student <students@themakersquare.com>
2013-12-31 18:04:39 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HonorWithAction::Application.config.secret_key_base = 'f054fa8c2b79f68d9a15d78a707e033c2b2efb3fc190789c881a08eb9c2f3e39f033f1885f75b8d138d27eec40ce18e166c11d0d83f2508b8f33af78567ca191'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HonorWithAction::Application.config.session_store :cookie_store, key: '_honor_with_action_session'
